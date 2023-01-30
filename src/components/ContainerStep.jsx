8dc3ff34b8ebb43027d6565259a938a3dc4da951
kewinbarboza <kewinbarboza@gmail.com>
2023-01-30 18:45:12 -0400

    new component step form

---
+++ src/components/ContainerStep.jsx
@@ -0,0 +1,10 @@
+export default function ContainerStep({ children, step }) {
+	return (
+		<section className='relative mb-6 flex flex-1 items-start duration-500'>
+			<span className='mr-3 flex h-8 w-8 items-center justify-center rounded-full bg-[#1d4fd846] text-center font-bold text-blue-700'>
+				{step}
+			</span>
+			{children}
+		</section>
+	)
+}
