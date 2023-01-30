139875af404333f7096037b8649d69514ab5e0c2
kewinbarboza <kewinbarboza@gmail.com>
2023-01-30 18:41:41 -0400

    change lenguaje

--- src/constants/tone.js
+++ src/constants/tone.js
@@ -1,65 +1,65 @@
 export const TONE = [
-  {
-    title: 'Amigable',
-    icon: '😊',
-    prompt: 'friendly',
-    name: 'tone',
-    id: 'amigable'
-  },
-  {
-    title: 'Lujo',
-    icon: '💎',
-    prompt: 'luxury',
-    name: 'tone',
-    id: 'lujo'
-  },
-  {
-    title: 'Relajado',
-    icon: '😌',
-    prompt: 'relaxed',
-    name: 'tone',
-    id: 'relajado'
-  },
-  {
-    title: 'Profesional',
-    icon: '💼',
-    prompt: 'professional',
-    name: 'tone',
-    id: 'profesional'
-  },
-  {
-    title: 'Atrevido',
-    icon: '💪',
-    prompt: 'bold',
-    name: 'tone',
-    id: 'atrevido'
-  },
-  {
-    title: 'Aventurero',
-    icon: '🚵‍♀️',
-    prompt: 'adventurous',
-    name: 'tone',
-    id: 'aventurero'
-  },
-  {
-    title: 'Ingenioso',
-    icon: '💡',
-    prompt: 'witty',
-    name: 'tone',
-    id: 'ingenioso'
-  },
-  {
-    title: 'Persuasivo',
-    icon: '🧠',
-    prompt: 'persuasive',
-    name: 'tone',
-    id: 'persuasivo'
-  },
-  {
-    title: 'Empático',
-    icon: '🤗',
-    prompt: 'empathetic',
-    name: 'tone',
-    id: 'empático'
-  }
+	{
+		title: 'Friendly',
+		icon: '😊',
+		prompt: 'friendly',
+		name: 'tone',
+		id: 'amigable'
+	},
+	{
+		title: 'Luxury',
+		icon: '💎',
+		prompt: 'luxury',
+		name: 'tone',
+		id: 'lujo'
+	},
+	{
+		title: 'Relaxed',
+		icon: '😌',
+		prompt: 'relaxed',
+		name: 'tone',
+		id: 'relajado'
+	},
+	{
+		title: 'Professional',
+		icon: '💼',
+		prompt: 'professional',
+		name: 'tone',
+		id: 'profesional'
+	},
+	{
+		title: 'Bold',
+		icon: '💪',
+		prompt: 'bold',
+		name: 'tone',
+		id: 'atrevido'
+	},
+	{
+		title: 'Adventurous',
+		icon: '🚵‍♀️',
+		prompt: 'adventurous',
+		name: 'tone',
+		id: 'aventurero'
+	},
+	{
+		title: 'Witty',
+		icon: '💡',
+		prompt: 'witty',
+		name: 'tone',
+		id: 'ingenioso'
+	},
+	{
+		title: 'Persuasive',
+		icon: '🧠',
+		prompt: 'persuasive',
+		name: 'tone',
+		id: 'persuasivo'
+	},
+	{
+		title: 'Empathetic',
+		icon: '🤗',
+		prompt: 'empathetic',
+		name: 'tone',
+		id: 'empático'
+	}
 ]
