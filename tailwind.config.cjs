f631798f7a1d41aa5df2a384ccee7d22eb098013
kewinbarboza <kewinbarboza@gmail.com>
2023-01-30 18:41:04 -0400

    change ' for "

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: 'class',
 	theme: {
-		extend: {},
+		extend: {}
 	},
-	plugins: [],
+	plugins: []
 }
