f238b6819e759be34d430fd1afd2cdf1c4a60591
kewinbarboza <kewinbarboza@gmail.com>
2023-01-30 18:45:49 -0400

    function copy email

--- src/components/ListEmails.jsx
+++ src/components/ListEmails.jsx
@@ -1,37 +1,46 @@
-export function ListEmails ({ content }) {
-  if (!content) return null
+import { useEffect, useState } from 'react'
 
-  return (
-    <>
-      <h3 className='mt-9 px-52 text-center text-xl font-medium text-gray-900 dark:text-white'>
-        Resultados
-      </h3>
-      <ul>
-        {content.map((c, index) => (
-          <li key={index} className='py-10 border-y-2 px-3 border-gray-600'>
-            <pre
-              contentEditable='true'
-              className='text-lg break-normal font-inherit whitespace-pre-wrap cursor-text focus:outline-none focus:ring p-3 text-gray-500 border-2 border-gray-900 rounded-lg dark:hover:text-gray-300 hover:text-gray-600 dark:text-gray-400 dark:hover:bg-gray-700'
-            >
-              {c}
-            </pre>
-            <div className='mt-4 gap-2'>
-              <button
-                type='button'
-                className='text-gray-700 hover:text-white border-2 border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2 text-center mr-2 mb-2 dark:border-blue-500 dark:text-white dark:hover:text-white dark:hover:bg-blue-600 dark:focus:ring-blue-800'
-              >
-                Copiar
-              </button>
-              <button
-                type='button'
-                className='text-gray-700 hover:text-white border-2 border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2 text-center mr-2 mb-2 dark:border-blue-500 dark:text-white dark:hover:text-white dark:hover:bg-blue-600 dark:focus:ring-blue-800'
-              >
-                Guardar
-              </button>
-            </div>
-          </li>
-        ))}
-      </ul>
-    </>
-  )
+export function ListEmails({ content }) {
+	const [copy, setCopy] = useState('Copy')
+
+	const copyEmail = (text) => {
+		if (navigator && navigator.clipboard && navigator.clipboard.writeText) {
+			setCopy('Copied!')
+			setTimeout(() => setCopy('Copy'), 3000)
+			return navigator.clipboard.writeText(text)
+		}
+		return Promise.reject('The Clipboard API is not available.')
+	}
+
+	if (!content) return ''
+
+	return (
+		<section>
+			{content && (
+				<>
+					<h3 className='mt-9 px-52 text-center text-xl font-medium text-gray-900 dark:text-white'>
+						Generated mail result
+					</h3>
+					<article className='py-10 px-3'>
+						<pre
+							suppressContentEditableWarning={true}
+							contentEditable='true'
+							className='font-inherit cursor-text whitespace-pre-wrap break-normal rounded-lg border border-gray-100 p-3  text-lg text-gray-500 hover:bg-gray-200 hover:text-gray-600 focus:outline-none focus:ring dark:border-gray-600 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300'
+						>
+							{content}
+						</pre>
+						<div className='mt-4 gap-2'>
+							<button
+								onClick={() => copyEmail(content)}
+								type='button'
+								className='mr-2 mb-2 rounded-lg bg-gray-100 px-5 py-1 text-center text-sm text-gray-700 shadow-md hover:bg-blue-800 hover:text-white focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-gray-700 dark:text-white  dark:shadow-sm dark:hover:bg-blue-600 dark:hover:text-white dark:focus:ring-blue-800'
+							>
+								{copy}
+							</button>
+						</div>
+					</article>
+				</>
+			)}
+		</section>
+	)
 }
