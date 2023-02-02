335ca1c53bbef26d1df389137b270bbf796140a2
kewinbarboza <kewinbarboza@gmail.com>
2023-02-02 15:26:01 -0400

    add vercel config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,17 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-import tailwind from '@astrojs/tailwind'
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
-import react from '@astrojs/react'
+import react from '@astrojs/react';
+
+// https://astro.build/config
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-	output: 'server',
-	integrations: [tailwind(), react()]
-})
+  output: 'server',
+  integrations: [tailwind(), react()],
+  adapter: vercel()
+});
